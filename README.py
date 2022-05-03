0ed0be40cb3127c8a3bdf331a7152da0882b5439
HiramHernandez <hhg.mzt@gmail.com>
2022-05-03 16:19:58 -0600

    Update README.py

---
+++ README.py
@@ -0,0 +1,14 @@
+## Cash calculator
+This example was builde with python 3.8 and doesn't need any dependencie
+So in this case doesn't need create a virtualenv
+
+Steps to execute the project
+
+ 1. Download this repo git clone https://github.com/HiramHernandez/cash-calculate.git
+ 2. In root of the project execute:
+	  `python cash.py`
+ 3. In terminal you see a message Change owed and you should type a mount that means the change is owed to a customer. If you type a invalid or negative number the program again show the message. If you typed a correct mount the program shows the minimal coins of a certain denomintaion
+ 4. There is a script with test case if you want to execute the tests you should execute in root the next command:
+ `python tests.py`
+Happy coding..
+Thanks for reading.
